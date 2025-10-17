5d279f0eeb69271387892aa219b2797a9dd86167
Fernando Cueto <fernandoecueto@gmail.com>
2025-10-18 01:29:47 +0200

    Refactor PlanosComponent styles: adjust label font sizes and input padding for improved readability

--- src/components/ui/PlanosComponent.vue
+++ src/components/ui/PlanosComponent.vue
@@ -71,26 +71,26 @@
       />
       <label 
         :htmlFor="titulo"
-        className="text-sm font-medium text-foreground cursor-pointer"
+        className="text-xs font-medium text-foreground cursor-pointer"
       >
         {{ titulo }}
       </label>
 
       <div v-if="showDescription" className="ml-6 flex items-center gap-4">
-        <label :htmlFor="descripcion" className="block text-sm font-medium text-foreground">
-          Descripción
+        <label :htmlFor="descripcion" className="block text-xs font-medium text-foreground">
+          Descripción:
         </label>
         <input 
           id="descripcion"
           type="text"
           placeholder="Descripción del plano"
-          className="max-w-md px-3 py-1 bg-background border border-gray-300 rounded-md placeholder:text-gray-400  focus-visible:outline-indigo-600 sm:text-sm/6"
+          className="max-w-md px-3 py-0.5 bg-background border border-gray-300 rounded-md placeholder:text-gray-400 focus-visible:outline-indigo-600 sm:text-sm"
           v-model="descripcion"
         />
       </div>
     </div>
     <div className="ml-6 flex items-center gap-4">
-        <label :htmlFor="titulo" className="block text-sm font-medium text-foreground">
+        <label :htmlFor="titulo" className="block text-xs font-medium text-foreground">
           Número de Planos
         </label>
         <input 
@@ -98,7 +98,7 @@
           type="number"
           min="0"
           placeholder="0"
-          className="max-w-20 px-3 py-1 bg-background border border-gray-300 rounded-md placeholder:text-gray-400  focus-visible:outline-indigo-600 sm:text-sm/6"
+          className="max-w-20 px-3 py-0.5 bg-background border border-gray-300 rounded-md placeholder:text-gray-400 focus-visible:outline-indigo-600 sm:text-sm"
           v-model="numeroPlanos"
         />
       </div>
